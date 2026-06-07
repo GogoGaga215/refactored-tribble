182ce6f16573b2007f9d95d9b8575d44c4edfccf
GogoGaga215 <carlokimlusterio112@gmail.com>
2026-06-07 09:00:21 +0800

    Add decryption logic for encrypted script data

--- pages/api/run/[id].js
+++ pages/api/run/[id].js
@@ -16,9 +16,40 @@ export default async function handler(req, res) {
     }
 
     const script = JSON.parse(data)
+    const encrypted = script.encryptedData
+
+    const decryptor = `
+local HttpService = game:GetService("HttpService")
+local function xor(a, b)
+    local r = 0
+    local p = 1
+    while a > 0 or b > 0 do
+        local ab = a % 2
+        local bb = b % 2
+        if ab ~= bb then r = r + p end
+        p = p * 2
+        a = math.floor(a / 2)
+        b = math.floor(b / 2)
+    end
+    return r
+end
+local function decrypt(b64)
+    local data = HttpService:Base64Decode(b64)
+    local key = string.byte(data, 1)
+    local enc = string.sub(data, 2)
+    local out = ""
+    for i = 1, #enc do
+        out = out .. string.char(xor(string.byte(enc, i), key))
+    end
+    return out
+end
+local code = decrypt("${encrypted}")
+loadstring(code)()
+`
+
     res.setHeader('Content-Type', 'text/plain')
     res.setHeader('Cache-Control', 'no-store')
-    res.send(script.encryptedData)
+    res.send(decryptor)
   } catch (err) {
     res.status(500).send('print("Error: ' + err.message + '")')
   }
